e88780af2eadfe9c9625c952c4e7391504f22215
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-08-19 00:00:23 -0700

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -13,6 +13,27 @@ echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
 
+cat <<EOF > /root/testaudit
+type=AVC msg=audit(1755585054.754:548): avc:  denied  { connectto } for  pid=9430 comm="rhsm-service" path="/run/systemd/userdb/io.systemd.Machine" scontext=system_u:system_r:rhsmcertd_t:s0 tcontext=system_u:system_r:systemd_machined_t:s0 tclass=unix_stream_socket permissive=0
+
+        Was caused by:
+        The boolean daemons_enable_cluster_mode was set incorrectly. 
+        Description:
+        Allow daemons to enable cluster mode
+
+        Allow access by executing:
+        # setsebool -P daemons_enable_cluster_mode 1
+type=AVC msg=audit(1755585054.755:549): avc:  denied  { connectto } for  pid=9430 comm="rhsm-service" path="/run/systemd/userdb/io.systemd.Machine" scontext=system_u:system_r:rhsmcertd_t:s0 tcontext=system_u:system_r:systemd_machined_t:s0 tclass=unix_stream_socket permissive=0
+
+        Was caused by:
+        The boolean daemons_enable_cluster_mode was set incorrectly. 
+        Description:
+        Allow daemons to enable cluster mode
+
+        Allow access by executing:
+        # setsebool -P daemons_enable_cluster_mode 1
+EOF
+
 #echo "Adding wheel" > /root/post-run.log
 #usermod -aG wheel rhel
 
